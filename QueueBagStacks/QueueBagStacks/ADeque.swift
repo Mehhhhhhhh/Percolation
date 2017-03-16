e11f6bff426046a0f54db0c898860b3710af98c4
alebla26 <alex19b@gmail.com>
2017-03-15 20:40:04 -0500

    Getting balanced

---
+++ QueueBagStacks/QueueBagStacks/ADeque.swift
@@ -0,0 +1,38 @@
+//
+//  ADeque.swift
+//  QueueBagStacks
+//
+//  Created by Alex Blanchard on 3/15/17.
+//  Copyright © 2017 ForeCyte. All rights reserved.
+//
+
+import Foundation
+struct Deque<T>{
+  
+  var _item: T
+  var _head: T
+  var _tail: T
+  
+  init(item: T, followedBy: () -> T) {
+    _item = item
+    
+  }
+  
+  func addFirst() {
+    
+  }
+  
+  func  addLast() {
+    
+  }
+  
+  func removeFirst() {
+    
+  }
+  
+  func removeLast() {
+    
+  }
+  
+  
+}
